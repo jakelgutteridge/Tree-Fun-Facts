6b803f1532eea2c91275a054685f59cd73447981
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-11 00:04:00 +0100

    Update breadcrumb.js

--- breadcrumb.js
+++ breadcrumb.js
@@ -13,12 +13,22 @@ function generateBreadcrumb() {
     breadcrumb.innerHTML = breadcrumbHTML;
 }
 
-function getTitleFromMeta(segment, isLastSegment) {
+async function getTitleFromMeta(segment, isLastSegment) {
     if (segment.endsWith('.html')) {
         return ` &gt; <a href="${segment}"${isLastSegment ? ' class="current"' : ''}>${document.title}</a>`;
     } else {
-        return ` &gt; <a href="${segment + '/index.html'}"${isLastSegment ? ' class="current"' : ''}>${segment}</a>`;
+        const title = await getTitleFromIndex(segment);
+        return ` &gt; <a href="${segment + '/index.html'}"${isLastSegment ? ' class="current"' : ''}>${title}</a>`;
     }
+}
+
+async function getTitleFromIndex(directory) {
+    const response = await fetch(directory + '/index.html');
+    const text = await response.text();
+    const titleTagStart = text.indexOf('<title>');
+    const titleTagEnd = text.indexOf('</title>', titleTagStart);
+    const title = text.substring(titleTagStart + 7, titleTagEnd);
+    return title;
 }
 
 window.onload = generateBreadcrumb;
