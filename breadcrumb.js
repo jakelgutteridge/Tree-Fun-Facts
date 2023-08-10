e7ab78c9f3b1c5f48441d0cde20017d0fbb4a05e
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-10 23:59:52 +0100

    Update breadcrumb.js

--- breadcrumb.js
+++ breadcrumb.js
@@ -5,36 +5,20 @@ function generateBreadcrumb() {
     const segments = url.split('/').filter(segment => segment !== '');
 
     let breadcrumbHTML = '<a href="/">Home</a>';
-    let path = '/';
 
-    const promises = segments.map(async (segment, index) => {
-        path += segment + '/';
-        const title = await getTitleFromMeta(path);
-        breadcrumbHTML += ` &gt; <a href="${path}"${index === segments.length - 1 ? ' class="current"' : ''}>${title}</a>`;
-    });
-
-    Promise.all(promises)
-        .then(() => {
-            breadcrumb.innerHTML = breadcrumbHTML;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-async function getTitleFromMeta(path) {
-    const response = await fetch(path + 'index.html');
-    if (response.ok) {
-        const text = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(text, 'text/html');
-        const titleElement = doc.querySelector('title');
-        if (titleElement) {
-            return titleElement.textContent;
-        }
+    for (let i = 0; i < segments.length; i++) {
+        breadcrumbHTML += getTitleFromMeta(segments[i], i === segments.length - 1);
     }
 
-    return 'Untitled';
+    breadcrumb.innerHTML = breadcrumbHTML;
+}
+
+function getTitleFromMeta(segment, isLastSegment) {
+    if (segment.endsWith('.html')) {
+        return ` &gt; <a href="${segment}"${isLastSegment ? ' class="current"' : ''}>${document.title}</a>`;
+    } else {
+        return ` &gt; <a href="${segment + '/index.html'}"${isLastSegment ? ' class="current"' : ''}>${segment}</a>`;
+    }
 }
 
 window.onload = generateBreadcrumb;
