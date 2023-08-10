ddfad0c864bcbc49e5f9c635116b3870da45f4a7
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-10 23:22:28 +0100

    Add files via upload

--- breadcrumb.js
+++ breadcrumb.js
@@ -4,22 +4,20 @@ function generateBreadcrumb() {
     const url = window.location.pathname;
     const segments = url.split('/').filter(segment => segment !== '');
 
-    let breadcrumbHTML = '<a href="/">Home</a> &gt; ';
-    let path = '/';
+    let breadcrumbHTML = '<a href="/">Home</a>';
 
-    segments.forEach(segment => {
-        path += segment + '/';
-        breadcrumbHTML += getTitleFromMeta(path, segment);
-    });
+    for (let i = 0; i < segments.length; i++) {
+        breadcrumbHTML += getTitleFromMeta(segments[i], i === segments.length - 1);
+    }
 
     breadcrumb.innerHTML = breadcrumbHTML;
 }
 
-function getTitleFromMeta(path, segment) {
+function getTitleFromMeta(segment, isLastSegment) {
     if (segment.endsWith('.html')) {
-        return ` &gt; <a href="${path}">${document.title}</a>`;
+        return ` &gt; <a href="${segment}"${isLastSegment ? ' class="current"' : ''}>${document.title}</a>`;
     } else {
-        return ` &gt; <a href="${path}">${segment}</a>`;
+        return ` &gt; <a href="${segment + '/index.html'}"${isLastSegment ? ' class="current"' : ''}>${segment}</a>`;
     }
 }
 
