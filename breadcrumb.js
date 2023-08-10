b51d73ea58db385a062ffdca2177f4379a5a1eb2
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-10 23:50:27 +0100

    Update breadcrumb.js

--- breadcrumb.js
+++ breadcrumb.js
@@ -10,10 +10,31 @@ function generateBreadcrumb() {
     for (let i = 0; i < segments.length; i++) {
         const segment = segments[i];
         path += segment + '/';
-
-        breadcrumbHTML += ` &gt; <a href="${path}"${i === segments.length - 1 ? ' class="current"' : ''}>${document.title}</a>`;
-        breadcrumb.innerHTML = breadcrumbHTML;
+        
+        getTitleFromMeta(path, segment, i === segments.length - 1)
+            .then(title => {
+                breadcrumbHTML += ` &gt; <a href="${path}"${i === segments.length - 1 ? ' class="current"' : ''}>${title}</a>`;
+                breadcrumb.innerHTML = breadcrumbHTML;
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
+}
+
+async function getTitleFromMeta(path, segment, isLastSegment) {
+    const response = await fetch(path + 'index.html');
+    if (response.ok) {
+        const text = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(text, 'text/html');
+        const titleElement = doc.querySelector('title');
+        if (titleElement) {
+            return titleElement.textContent;
+        }
+    }
+
+    return isLastSegment ? segment : 'Untitled';
 }
 
 window.onload = generateBreadcrumb;
