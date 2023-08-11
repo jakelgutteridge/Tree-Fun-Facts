16b06dbcb9ab0c06a830935e88c57172832a3798
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-11 03:16:46 +0100

    Update breadcrumb.js

--- breadcrumb.js
+++ breadcrumb.js
@@ -1,33 +1,37 @@
         // Get the current URL path
         var path = window.location.pathname;
-        
+
         // Split the path into segments
         var segments = path.split('/').filter(function(segment) {
             return segment !== "";
         });
-        
+
         // Initialize the breadcrumb HTML
         var breadcrumbHtml = '<a href="/">Home</a> > ';
-        
+
         // Loop through the segments to create the breadcrumb
         var currentPath = "/";
         for (var i = 0; i < segments.length; i++) {
             currentPath += segments[i] + '/';
             var title = segments[i];
-            
+
             // Check if the segment is a directory and has an index.html
-            if (i === segments.length - 1 && segments[i].endsWith('.html')) {
-                title = '<strong>' + document.title + '</strong>';
-            } else if (i === segments.length - 1 && segments[i].endsWith('/')) {
-                title = '<strong>Index</strong>';
+            if (i === segments.length - 1) {
+                if (title === "") {
+                    title = "Home";
+                } else if (segments[i].endsWith('/')) {
+                    title = "Index";
+                } else {
+                    title = '<strong>' + document.title + '</strong>';
+                }
             }
-            
+
             breadcrumbHtml += '<a href="' + currentPath + '">' + title + '</a>';
-            
+
             if (i < segments.length - 1) {
                 breadcrumbHtml += ' > ';
             }
         }
-        
+
         // Display the breadcrumb
         document.getElementById('breadcrumb').innerHTML = breadcrumbHtml;
